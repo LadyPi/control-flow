647e68923e0ad8e16abae3e21f38937f8922b9e4
LadyPi <ms.vanessa.farias.dm@gmail.com>
2016-05-05 10:29:43 -0700

    condition exercises

---
+++ can_i_ride.js
@@ -0,0 +1,63 @@
+var height = 3;
+
+if (height >= 4) {
+	console.log("Step right up!");
+	} else {  
+	console.log("Sorry, you can't ride");
+}
+
+
+var age = 11;
+
+if (age >= 12) {
+	console.log("Step right up!");
+	} else {  
+	console.log("Sorry, you can't ride");
+}
+
+var age = 11;
+
+if (age < 12) {
+	console.log("You must be accompanied by a parent!");
+	} else {  
+	console.log("Step right up!");
+}
+
+var boss = "looking";
+
+if (boss == "looking") {
+	console.log("Sorry,  you can't ride!");
+	} else {  
+	console.log("Step right up!");
+}
+
+var parkPass = true;
+
+if (parkPass === true) {
+	console.log("Step right up!");
+	} else {  
+	console.log("Sorry, you can't ride");
+}
+
+var freePass = false;
+var height = 3;
+var age = 6;
+var boss = true;
+
+if (freePass === true && age >= 12 && height >= 4 || boss === false) {
+	console.log("Step right up!");
+} else {
+	console.log("Sorry, you can't ride!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
